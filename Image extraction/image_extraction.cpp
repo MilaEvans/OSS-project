615213e140d6a7fc266d35f9fb8132ded1e228a1
doseong :) <118369702+HappyThings2@users.noreply.github.com>
2025-05-19 14:09:58 +0900

    Update image_extraction.cpp

--- Image extraction/image_extraction.cpp
+++ Image extraction/image_extraction.cpp
@@ -18,4 +18,41 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+        std::string input((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+    infile.close();
+
+    input = clean(input); // 입력 문자열 공백 제거
+
+    std::vector<std::pair<std::string, std::string>> clubs = {
+        {"코딩", "코딩!!!!"},
+        {"Robot Club", "자율주행 로봇 개발"},
+        {"AI Club", "자율주행 인공지능 알고리즘 연구"},
+        {"Electric Vehicle Club", "전기차 및 자율주행차 연구"},
+        {"Computer Vision Club", "컴퓨터 비전과 영상 처리 연구"}
+    };
+
+    std::vector<std::string> matches;
+    for (auto& club : clubs) {
+        std::string keyword_clean = clean(club.first);
+        std::string desc_clean = clean(club.second);
+
+        if (input.find(keyword_clean) != std::string::npos ||
+            input.find(desc_clean) != std::string::npos) {
+            matches.push_back(club.first + ": " + club.second);
+        }
+    }
+
+    if (matches.empty()) {
+        std::cout << "No matching clubs found.\n";
+    }
+    else {
+        for (auto& m : matches) {
+            std::cout << m << "\n";
+        }
+    }
+
+    return 0;
+}
+
+
 }
