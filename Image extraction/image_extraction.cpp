8f9bd46646d3835903deae3b1170579404703996
doseong :) <118369702+HappyThings2@users.noreply.github.com>
2025-05-22 15:47:10 +0900

    다중 키워드 검색 추가

--- Image extraction/image_extraction.cpp
+++ Image extraction/image_extraction.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <cctype>
 
-// clean 함수 정의: 공백 제거 + 소문자 변환
+// clean 함수: 공백 제거 + 소문자 변환
 std::string clean(const std::string& str) {
     std::string result;
     for (char c : str) {
@@ -33,24 +33,31 @@ int main(int argc, char* argv[])
     std::string input((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
     infile.close();
 
-    input = clean(input); // 입력 문자열 공백 제거
+    input = clean(input);
 
-    std::vector<std::pair<std::string, std::string>> clubs = {
-        {"코딩", "코딩!!!!"},
-        {"Robot Club", "자율주행 로봇 개발"},
-        {"AI Club", "자율주행 인공지능 알고리즘 연구"},
-        {"Electric Vehicle Club", "전기차 및 자율주행차 연구"},
-        {"Computer Vision Club", "컴퓨터 비전과 영상 처리 연구"}
+    // 각 동아리마다 여러 개의 키워드(관련 단어) 등록
+    struct Club {
+        std::string name;
+        std::string description;
+        std::vector<std::string> keywords;
+    };
+
+    std::vector<Club> clubs = {
+        {"코딩", "코딩!!!!", {"코딩", "프로그래밍", "html", "css", "javascript", "python", "웹"}},
+        {"Robot Club", "자율주행 로봇 개발", {"로봇", "자율주행", "자동차", "기계"}},
+        {"AI Club", "자율주행 인공지능 알고리즘 연구", {"인공지능", "ai", "머신러닝", "딥러닝", "알고리즘"}},
+        {"Electric Vehicle Club", "전기차 및 자율주행차 연구", {"전기차", "전기", "배터리", "자율주행"}},
+        {"Computer Vision Club", "컴퓨터 비전과 영상 처리 연구", {"컴퓨터비전", "영상처리", "opencv", "이미지", "영상"}}
     };
 
     std::vector<std::string> matches;
-    for (auto& club : clubs) {
-        std::string keyword_clean = clean(club.first);
-        std::string desc_clean = clean(club.second);
 
-        if (input.find(keyword_clean) != std::string::npos ||
-            input.find(desc_clean) != std::string::npos) {
-            matches.push_back(club.first + ": " + club.second);
+    for (const auto& club : clubs) {
+        for (const auto& kw : club.keywords) {
+            if (input.find(clean(kw)) != std::string::npos) {
+                matches.push_back(club.name + ": " + club.description);
+                break;  // 키워드 하나라도 매칭되면 해당 동아리 추가하고 다음 동아리 검사
+            }
         }
     }
 
@@ -58,12 +65,10 @@ int main(int argc, char* argv[])
         std::cout << "No matching clubs found.\n";
     }
     else {
-        for (auto& m : matches) {
+        for (const auto& m : matches) {
             std::cout << m << "\n";
         }
     }
 
     return 0;
 }
-
-
